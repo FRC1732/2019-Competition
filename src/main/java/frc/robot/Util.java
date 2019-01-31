7785273c7c5f4c86528f41f84aab940429bc14ea
Matthew Pomes <the10thwiz@gmail.com>
2019-01-31 03:20:27 +0000

    Replace Tabs with Spaces

--- src/main/java/frc/robot/Util.java
+++ src/main/java/frc/robot/Util.java
@@ -5,37 +5,37 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Util {
-    /**
-     * Creates a TalonSRX object with the specified parameters, along with other
-     * default settings
-     * 
-     * @param id
-     *                     the CAN id of the talon
-     * @param reversed
-     *                     whether the motor should be reversed
-     * @return the TalonSRX object constructed
-     */
-    public static TalonSRX createTalon(int id, boolean reversed) {
-        TalonSRX motor = new TalonSRX(id);
-        motor.setInverted(reversed);
-        motor.setNeutralMode(NeutralMode.Brake);
-        return motor;
-    }
-    
-    /**
-     * Creates a VictorSPX object with the specified parameters, along with other
-     * default settings
-     * 
-     * @param id
-     *                     the CAN id of the talon
-     * @param reversed
-     *                     whether the motor should be reversed
-     * @return the VictorSPX object constructed
-     */
-    public static VictorSPX createVictor(int id, boolean reversed) {
-        VictorSPX motor = new VictorSPX(id);
-        motor.setInverted(reversed);
-        motor.setNeutralMode(NeutralMode.Brake);
-        return motor;
-    }
+  /**
+   * Creates a TalonSRX object with the specified parameters, along with other
+   * default settings
+   * 
+   * @param id
+   *                   the CAN id of the talon
+   * @param reversed
+   *                   whether the motor should be reversed
+   * @return the TalonSRX object constructed
+   */
+  public static TalonSRX createTalon(int id, boolean reversed) {
+    TalonSRX motor = new TalonSRX(id);
+    motor.setInverted(reversed);
+    motor.setNeutralMode(NeutralMode.Brake);
+    return motor;
+  }
+  
+  /**
+   * Creates a VictorSPX object with the specified parameters, along with other
+   * default settings
+   * 
+   * @param id
+   *                   the CAN id of the talon
+   * @param reversed
+   *                   whether the motor should be reversed
+   * @return the VictorSPX object constructed
+   */
+  public static VictorSPX createVictor(int id, boolean reversed) {
+    VictorSPX motor = new VictorSPX(id);
+    motor.setInverted(reversed);
+    motor.setNeutralMode(NeutralMode.Brake);
+    return motor;
+  }
 }
