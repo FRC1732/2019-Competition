ab48516618b40d93f3561d118b2318abae19224a
Matthew Pomes <the10thwiz@gmail.com>
2019-01-31 00:05:14 +0000

    Add warnings to Main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -11,17 +11,22 @@ import edu.wpi.first.wpilibj.RobotBase;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all.
- * Unless you know what you are doing, do not modify this file except to
- * change the parameter class to the startRobot call.
+ * Unless you know what you are doing, do not modify this file except to change
+ * the parameter class to the startRobot call.
+ * 
+ * DO NOT TOUCH THIS CODE!
  */
 public final class Main {
   private Main() {
   }
-
+  
   /**
    * Main initialization function. Do not perform any initialization here.
    *
-   * <p>If you change your main robot class, change the parameter type.
+   * <p>
+   * If you change your main robot class, change the parameter type.
+   * 
+   * DO NOT TOUCH THIS CODE!
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
