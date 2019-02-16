01d6aac898f7a68fbceaca463e7043759d52ea55
lauj10 <lauj10@muhs.edu>
2019-02-16 10:47:06 -0600

    Fix IDs

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -38,6 +38,6 @@ public class RobotMap {
   public static final int OI_OPERATOR_1_ID = 0;
   public static final int OI_OPERATOR_2_ID = 3;
 
-  public static final int CARGO_INTAKE_MOTOR_ID = 0;
-  public static final int CARGO_INTAKE_SOLENOID_ID = 0;
+  public static final int CARGO_INTAKE_MOTOR_ID = 6;
+  public static final int CARGO_INTAKE_SOLENOID_ID = 2;
 }
