7e8c44bda151c6365813272fcdfe0251d8a7ee45
Matthew Pomes <the10thwiz@gmail.com>
2019-02-16 16:38:05 +0000

    Add Jacks and logic

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,10 +7,15 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.util.MotorUtil;
 
 /**
- * Add your docs here.
  * 
  * 3 motors for jacks - limit switches
  * 
@@ -32,11 +37,96 @@ public class Climber extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   
+  private TalonSRX frontLeft = MotorUtil.createTalon(RobotMap.CLIMBER_FRONT_LEFT, false);
+  private TalonSRX frontRight = MotorUtil.createTalon(RobotMap.CLIMBER_FRONT_RIGHT, false);
+  private TalonSRX back = MotorUtil.createTalon(RobotMap.CLIMBER_BACK, false);
+  private VictorSPX driver = MotorUtil.createVictor(RobotMap.CLIMBER_DRIVER, false);
+  
+  /**
+   * Various positions for the jacks
+   */
+  private final double TOP = 1;
+  private final double BOTTOM = 0;
+  private final double ALLOWED_ERROR = 1;
+  
+  private double frontTarget = BOTTOM;
+  private double backTarget = BOTTOM;
+  
+  /**
+   * Stage on: 0: disabled 1: lift jacks 2: push forward 3: lower front 4: push
+   * forward 5: lower back 6: climbed
+   */
+  private int stage = 0;
+  
+  public Climber() {
+    
+  }
+  
   /**
    * Starts the Jacks raising
    */
   public void raiseJacks() {
-    
+    if (stage == 0) {
+      stage = 1;
+    }
+  }
+  
+  @Override
+  public void periodic() {
+    updateTargets();
+    setMotors();
+  }
+  
+  /**
+   * Checks the position of the motors, and sets a new target for them
+   */
+  private void updateTargets() {
+    switch (stage) {
+    case 1:
+      // lift jacks
+      if (isErrorAllowed()) {
+        frontTarget++;
+        backTarget++;
+        if (frontTarget >= TOP && backTarget >= TOP) {
+          stage = 2;
+        }
+      }
+    case 2:
+      // push forward
+    case 3:
+      // lower front
+      if (isErrorAllowed()) {
+        frontTarget--;
+        if (frontTarget <= BOTTOM && backTarget >= TOP) {
+          stage = 4;
+        }
+      }
+    case 4:
+      // push forward
+    case 5:
+      // lower back
+      if (isErrorAllowed()) {
+        backTarget--;
+        if (frontTarget <= BOTTOM && backTarget <= BOTTOM) {
+          stage = 6;
+        }
+      }
+    }
+  }
+  
+  /**
+   * Sets the motors to their current target
+   */
+  private void setMotors() {
+    frontLeft.set(ControlMode.Position, frontTarget);
+    frontRight.set(ControlMode.Position, frontTarget);
+    back.set(ControlMode.Position, backTarget);
+  }
+  
+  private boolean isErrorAllowed() {
+    return Math.abs(frontLeft.getSelectedSensorPosition(0) - frontTarget) < ALLOWED_ERROR
+        && Math.abs(frontRight.getSelectedSensorPosition(0) - frontTarget) < ALLOWED_ERROR
+        && Math.abs(back.getSelectedSensorPosition(0) - backTarget) < ALLOWED_ERROR;
   }
   
   @Override
