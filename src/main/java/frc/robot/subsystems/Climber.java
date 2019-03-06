c51f90d5fa98b84173d6c3f910128b7956e433d6
Matthew <the10thwiz@gmail.com>
2019-03-06 13:33:49 -0600

    Add Climbing Functionality

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,11 +8,17 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
 import frc.robot.RobotMap;
+import frc.robot.util.Console;
 import frc.robot.util.MotorUtil;
 
 /**
@@ -38,19 +44,28 @@ public class Climber extends Subsystem {
   // here. Call these from Commands.
   
   private TalonSRX frontLeft = MotorUtil.createTalon(RobotMap.CLIMBER_FRONT_LEFT, false);
-  private TalonSRX frontRight = MotorUtil.createTalon(RobotMap.CLIMBER_FRONT_RIGHT, false);
+  private TalonSRX frontRight = MotorUtil.createTalon(RobotMap.CLIMBER_FRONT_RIGHT, true);
   private TalonSRX back = MotorUtil.createTalon(RobotMap.CLIMBER_BACK, false);
   private VictorSPX driver = MotorUtil.createVictor(RobotMap.CLIMBER_DRIVER, false);
   
   /**
    * Various positions for the jacks
+   * 
+   * 1" ~= 620 ticks
+   * 
+   * Robot and Jacks on ground == 0
    */
-  private final double TOP = 1;
   private final double BOTTOM = 0;
-  private final double ALLOWED_ERROR = 1;
+  private final double LVL2 = 620 * 7;
+  private final double LVL3 = 620 * 20;
   
-  private double frontTarget = BOTTOM;
-  private double backTarget = BOTTOM;
+  private double bottom = BOTTOM;
+  private double top = LVL2;
+  private final double ALLOWED_ERROR = 200;
+  
+  private double frontTarget = bottom;
+  private double backTarget = bottom;
+  private double drive = 0;
   
   /**
    * Stage on: 0: disabled 1: lift jacks 2: push forward 3: lower front 4: push
@@ -58,16 +73,64 @@ public class Climber extends Subsystem {
    */
   private int stage = 0;
   
+  private double kP = 2.0;
+  private double kI = 0.0;
+  private double kD = 0.0;
+  
   public Climber() {
+    frontLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+    frontRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+    back.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
     
+    frontLeft.setSensorPhase(false);
+    frontRight.setSensorPhase(false);
+    back.setSensorPhase(false);
+    
+    frontLeft.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyOpen);
+    frontLeft.configReverseLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyOpen);
+    frontRight.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyOpen);
+    frontRight.configReverseLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyOpen);
+    back.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyOpen);
+    back.configReverseLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyOpen);
+    
+    frontLeft.setSelectedSensorPosition(0, 0, 0);
+    frontRight.setSelectedSensorPosition(0, 0, 0);
+    back.setSelectedSensorPosition(0, 0, 0);
+    
+    frontLeft.config_kP(0, kP);
+    frontRight.config_kP(0, kP);
+    back.config_kP(0, kP);
+    
+    frontLeft.config_kI(0, kI);
+    frontRight.config_kI(0, kI);
+    back.config_kI(0, kI);
+    
+    frontLeft.config_kD(0, kD);
+    frontRight.config_kD(0, kD);
+    back.config_kD(0, kD);
+    
+    frontLeft.configClosedLoopPeakOutput(0, 0.7);
+    frontRight.configClosedLoopPeakOutput(0, 0.7);
+    back.configClosedLoopPeakOutput(0, 0.7);
+    
+    frontLeft.setNeutralMode(NeutralMode.Brake);
+    frontRight.setNeutralMode(NeutralMode.Brake);
+    back.setNeutralMode(NeutralMode.Brake);
   }
   
-  /**
-   * Starts the Jacks raising
-   */
-  public void raiseJacks() {
-    if (stage == 0) {
-      stage = 1;
+  public void setClimbHeight(int platform) {
+    switch (platform) {
+    case 1:
+      Console.warn("HAB 1 doesn't need climber");
+      break;
+    case 2:
+      top = LVL2;
+      break;
+    case 3:
+      top = LVL3;
+      break;
+    default:
+      Console.warn("HAB " + platform + " doesn't exist");
     }
   }
   
@@ -78,14 +141,14 @@ public class Climber extends Subsystem {
   }
   
   public void stage2() {
-    if (stage == 0) {
-      stage = 1;
+    if (stage == 2) {
+      stage = 3;
     }
   }
   
   public void stage3() {
-    if (stage == 0) {
-      stage = 1;
+    if (stage == 4) {
+      stage = 5;
     }
   }
   
@@ -93,6 +156,8 @@ public class Climber extends Subsystem {
   public void periodic() {
     updateTargets();
     setMotors();
+    Console.graph("Climber", frontLeft.getSelectedSensorPosition(0), frontRight.getSelectedSensorPosition(0),
+        back.getSelectedSensorPosition(0));
   }
   
   /**
@@ -102,43 +167,97 @@ public class Climber extends Subsystem {
     switch (stage) {
     case 1:
       // lift jacks
-      if (isErrorAllowed()) {
-        frontTarget++;
-        backTarget++;
-        if (frontTarget >= TOP && backTarget >= TOP) {
-          stage = 2;
-        }
+      drive(false, false);
+      if (moveTo(top, top)) {
+        stage = 2;
       }
     case 2:
       // push forward
+      drive(true, false);
+      moveTo(top, top);
     case 3:
       // lower front
-      if (isErrorAllowed()) {
-        frontTarget--;
-        if (frontTarget <= BOTTOM && backTarget >= TOP) {
-          stage = 4;
-        }
+      drive(false, false);
+      if (moveTo(bottom, top)) {
+        stage = 4;
       }
     case 4:
       // push forward
+      drive(true, true);
+      moveTo(bottom, top);
     case 5:
       // lower back
-      if (isErrorAllowed()) {
-        backTarget--;
-        if (frontTarget <= BOTTOM && backTarget <= BOTTOM) {
-          stage = 6;
-        }
+      drive(false, false);
+      if (moveTo(bottom, bottom)) {
+        stage = 6;
       }
     }
+  }
+  
+  private boolean moveTo(double front, double back) {
+    if (isErrorAllowed()) {
+      if (front > frontTarget) {
+        frontTarget += ALLOWED_ERROR;
+      } else if (front < frontTarget) {
+        frontTarget -= ALLOWED_ERROR;
+      }
+      if (back > backTarget) {
+        backTarget += ALLOWED_ERROR;
+      } else if (back < backTarget) {
+        backTarget -= ALLOWED_ERROR;
+      }
+      return Math.abs(frontTarget - front) <= 0 && Math.abs(backTarget - back) <= 0;
+    } else {
+      return false;
+    }
+  }
+  
+  private void drive(boolean back, boolean drivetrian) {
+    if (back) {
+      drive = 0.2;
+    } else {
+      drive = 0.0;
+    }
+    if (drivetrian) {
+      Robot.drivetrain.set(0.1, 0.1);
+    } else {
+      Robot.drivetrain.set(0.0, 0.0);
+    }
+  }
+  
+  private void holdJacks() {
+    if (frontLeft.getSelectedSensorPosition(0) > 400) {
+      frontLeft.set(ControlMode.PercentOutput, -.1);
+    } else {
+      frontLeft.set(ControlMode.PercentOutput, 0);
+    }
+    if (frontRight.getSelectedSensorPosition(0) > 400) {
+      frontRight.set(ControlMode.PercentOutput, -.1);
+    } else {
+      frontRight.set(ControlMode.PercentOutput, 0);
+    }
+    if (back.getSelectedSensorPosition(0) > 400) {
+      back.set(ControlMode.PercentOutput, -.1);
+    } else {
+      back.set(ControlMode.PercentOutput, 0);
+    }
+    driver.set(ControlMode.PercentOutput, 0.0);
   }
   
   /**
    * Sets the motors to their current target
    */
   private void setMotors() {
-    frontLeft.set(ControlMode.Position, frontTarget);
-    frontRight.set(ControlMode.Position, frontTarget);
-    back.set(ControlMode.Position, backTarget);
+    if (stage <= 1) {
+      holdJacks();
+      frontTarget = 0;
+      backTarget = 0;
+    } else {
+      frontLeft.set(ControlMode.Position, frontTarget);
+      frontRight.set(ControlMode.Position, frontTarget);
+      back.set(ControlMode.Position, backTarget);
+      driver.set(ControlMode.PercentOutput, drive);
+    }
   }
   
   private boolean isErrorAllowed() {
@@ -150,10 +269,11 @@ public class Climber extends Subsystem {
   @Override
   public void initDefaultCommand() {
   }
-
+  
   public void stop() {
     stage = 0;
   }
+  
   public void zero() {
   }
 }
