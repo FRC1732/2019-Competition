38ca4b22f7a6259c9ab2fd3c29004e33b53f6937
Matthew Pomes <the10thwiz@gmail.com>
2019-03-08 22:25:52 +0000

    copy angle

--- src/main/java/frc/robot/commands/auto/TurnToTarget.java
+++ src/main/java/frc/robot/commands/auto/TurnToTarget.java
@@ -7,13 +7,19 @@
 
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
+import frc.robot.subsystems.Limelight;
 
 /**
  * A command to turn the robot towards the nearest vision target
  */
 public class TurnToTarget extends Command {
+  private PIDController anglePID;
+  private PIDController forwardPID;
+  
   /**
    * A command to turn the robot towards the nearest vision target
    */
@@ -21,16 +27,35 @@ public class TurnToTarget extends Command {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
     requires(Robot.drivetrain);
+    anglePID = new PIDController(0.02, 0.0, 0.0, Robot.limelight.getHorizontalPidSource(), this::output);
+    anglePID.setOutputRange(-1, 1);
+    anglePID.setSetpoint(0);
+    SmartDashboard.putData("VisionTurning", anglePID);
+    // forwardPID = new PIDController(0.02, 0.0, 0.0,
+    // Robot.limelight.getDistancePidSource(Limelight.Target.AnyOther),
+    // this::output);
+    // forwardPID.setOutputRange(-1, 1);
+    // forwardPID.setSetpoint(20);
+    SmartDashboard.putData("VisionForward", forwardPID);
+  }
+  
+  private void output(double d) {
   }
   
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    anglePID.reset();
+    anglePID.enable();
   }
   
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double speed = (Robot.oi.getLeftJoystick() + Robot.oi.getRightJoystick()) * 0.5;
+    double turn = anglePID.get();
+    // System.out.printf("%1.3f /\\, > %1.3f\n", speed, turn);
+    Robot.drivetrain.set(speed - turn, speed + turn);
   }
   
   // Make this return true when this Command no longer needs to run execute()
@@ -42,11 +67,13 @@ public class TurnToTarget extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    anglePID.disable();
   }
   
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    anglePID.disable();
   }
 }
