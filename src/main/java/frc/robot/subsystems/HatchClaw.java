96aba25cc96ff08df57f373f8e9869930c4741b8
Matthew <the10thwiz@gmail.com>
2019-03-22 20:37:44 -0500

    Organize Imports

--- src/main/java/frc/robot/subsystems/HatchClaw.java
+++ src/main/java/frc/robot/subsystems/HatchClaw.java
@@ -9,9 +9,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.Robot;
 import frc.robot.RobotMap;
-import frc.robot.util.Console;
 
 /**
  * Add your docs here.
