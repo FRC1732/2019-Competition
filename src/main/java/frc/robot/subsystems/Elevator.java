bfd73ab4bf1fbc16f2374dc76449bd6a1c2d50f5
Matthew <the10thwiz@gmail.com>
2019-03-23 14:04:40 -0500

    Adjust Elevator Offset

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -36,7 +36,7 @@ public class Elevator extends Subsystem {
   private TalonSRX elevator = MotorUtil.initAbsoluteTalon(RobotMap.ELEVATOR_ELEVATOR_ID, false, true);
   private DigitalInput limit = new DigitalInput(0);
   
-  private final static int OFFSET = 1600;//1483;
+  private final static int OFFSET = 960;//1483;
   
   private static final double minimumOutput = .065;
   private static final double speed = 588;
