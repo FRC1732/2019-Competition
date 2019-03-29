a4897e09cfe181cdcd00b98b58a87b686bf5001e
Matthew <the10thwiz@gmail.com>
2019-03-28 19:01:22 -0500

    Adjust Offset

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -36,7 +36,7 @@ public class Elevator extends Subsystem {
   private TalonSRX elevator = MotorUtil.initAbsoluteTalon(RobotMap.ELEVATOR_ELEVATOR_ID, false, true);
   private DigitalInput limit = new DigitalInput(0);
   
-  private final static int OFFSET = 2524;
+  private final static int OFFSET = 2224;
   
   private static final double minimumOutput = .065;
   private static final double speed = 588;
@@ -83,7 +83,7 @@ public class Elevator extends Subsystem {
   public void setHeight(Position pos) {
     // passes the constant int for the height requested
     elevator.set(ControlMode.MotionMagic, pos.position, DemandType.ArbitraryFeedForward, minimumOutput);
-    Console.debug(String.format("set elevator height to: %5.5f", position));
+    // Console.debug(String.format("set elevator height to: %5.5f", position));
   }
   
   public int getHeight() {
@@ -106,7 +106,7 @@ public class Elevator extends Subsystem {
     //   Console.debug("elevator: " + elevator.getSelectedSensorPosition() + ", set to " + (position + OFFSET));
     //   elevator.set(ControlMode.MotionMagic, position, DemandType.ArbitraryFeedForward, 0);// minimumOutput);
     // }
-    Console.debug("Out: " + elevator.getMotorOutputPercent());
+    // Console.debug("Out: " + elevator.getMotorOutputPercent());
   }
   
   @Override
