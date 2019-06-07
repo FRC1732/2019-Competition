4925761ae754728ca46dfd07d39c5eb4168fb5fd
ProbablyAProgrammer <visayc15@muhs.edu>
2019-06-06 19:50:33 -0500

    Adding other bot's encoder values in comments

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -36,7 +36,7 @@ public class Elevator extends Subsystem {
   private TalonSRX elevator = MotorUtil.initAbsoluteTalon(RobotMap.ELEVATOR_ELEVATOR_ID, false, true);
   private DigitalInput limit = new DigitalInput(0);
   
-  private final static int OFFSET = 900; //3000
+  private final static int OFFSET = 3000; //900
   
   private static final double minimumOutput = .065;
   private static final double speed = 588;
